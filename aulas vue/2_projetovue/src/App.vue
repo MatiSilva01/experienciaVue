ee528de739109bd143d841d74000f26f19c37730
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-24 20:36:24 +0000

    ciclos de vida

--- aulas vue/2_projetovue/src/App.vue
+++ aulas vue/2_projetovue/src/App.vue
@@ -2,16 +2,19 @@
 <div>
   <p> Estamos a fazer um {{ testando }} </p>
 <primeiro-componente />
+<LifeCycle />
 </div>
 </template>
 <!--para ele saber, como no main.js -->
 <script>
 //vai importar os componentes p estar diponivel
 import primeiroComponente from './components/primeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 export default {
   name: 'App',
   components: { //os componentes vue que vou utilizar
-    primeiroComponente
+    primeiroComponente,
+    LifeCycle
   },
   data() {
     return {
---
+++ aulas vue/2_projetovue/src/components/LifeCycle.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>Meu nome é {{ nome }} </h1>
+</template>
+<script>
+
+export default ({
+    name: 'LifeCycle',
+    data(){
+        return{
+            //pq queremos que seja algo dinamico
+            //qaundo abrimos define o nome como ainda nao sei
+            nome: 'Ainda nao sei'
+        }
+    },
+    created(){
+        //Chamado depois que a instância concluiu o processamento de todas as opções relacionadas ao estado.
+        //quando e criado faz isso, define o nome Matilde
+        //settimeout aconece depois do mounted, no caso 1seg depois
+        setTimeout(() => {
+        //passado 1 seg muda o nome para Matilde
+        this.nome = 'Matilde'
+        }, 1000);
+    },
+    //Chamado depois que o componente foi montado
+    mounted(){
+        setTimeout(() => {
+        //passado 3 seg muda o nome para Matilde
+        this.nome = 'Pedro'
+        }, 3000);
+    }
+})
+</script>
