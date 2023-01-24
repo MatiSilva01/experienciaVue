64f11593b43b408b7675cf547b5ba4a5c4142858
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-24 15:39:37 +0000

    componentes e CLI

---
+++ aulas vue/2_projetovue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<primeiro-componente />
+</template>
+<!--para ele saber, como no main.js -->
+<script>
+//vai importar os componentes p estar diponivel
+import primeiroComponente from './components/primeiroComponente.vue'
+export default {
+  name: 'App',
+  components: { //os componentes vue que vou utilizar
+    primeiroComponente
+  }
+}
+</script>
