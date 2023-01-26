95f02da4c068f51b36be6d9e3a08fcb368d38066
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-26 19:00:39 +0000

    submit form

--- aulas vue/2_projetovue/src/components/Form.vue
+++ aulas vue/2_projetovue/src/components/Form.vue
@@ -1,23 +1,50 @@
 <template>
 <h2>Meu Formulario </h2>
-    <form action="">
+<!--como no java quando fazemos submit, chama o metodo enviarFormulario
+leva o event para nao dar bosta e meter la o preventdefault-->
+    <form action="" @submit="enviarFormulario($event)">
         <div>
-        <InputText />
+        <input type="text" :v-model="name">
+        </div>
+        <div>
+        <input type="text" :v-model="email">
         </div>
         <div>
         <Submit />
         </div>
     </form>
+    <p> Content </p>
 </template>
 <script>
-import InputText from './form/InputText.vue'
 import Submit from './form/Submit.vue'
 
 export default {
     name: 'FormF',
     components:{
-        InputText,
         Submit
+    }, 
+    data() {
+        return {
+            //dados do input, inicialmente a vazio
+            name: "",
+            email: ""
+        }
+    },
+    methods: {
+        enviarFormulario(e) {
+            //ignora o evento padrao 
+            e.preventDefault();
+            //atualiza os valores consoante os que o utilizador pos 
+            const name = this.name;
+            const email = this.email;
+
+            console.log("Nome " + name + " mail " + email)
+        }
     }
 }
 </script>
+<style scoped>
+p {
+    margin: 150px;
+}
+</style>
