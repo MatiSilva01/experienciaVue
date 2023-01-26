59cd1c594c1504548c00f0f14965b9fa084b39ef
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-26 18:34:39 +0000

    v-for

--- aulas vue/2_projetovue/src/components/Info.vue
+++ aulas vue/2_projetovue/src/components/Info.vue
@@ -4,12 +4,20 @@
     <p v-if="esta_trabalhando">Estou trabalhando no momento </p>
     <p v-else>Esta desempregado</p>
         <!--o else tem de estar em baixo do if -->
-<p> Utilizo as seguintes tecnologias </p>
+<p> Utilizo as seguintes tecnologias p beackend: </p>
+<!--um for para por as tecnologias -->
+<!-- metemos o index p servir de ID e ser unico -->
 <ul>
-<li>JavaScript</li>
-<li>Python</li>
-<li>Java</li>
-<li>SQL</li>
+    <!-- metemos o for no elemento que vai ser repetido -->
+    <!--fizemos assim pq e um array de strings -->
+    <li v-for="(tec, index) in backend" v-bind:key="index">{{tec}}</li>
+</ul>
+<p>Utilizo as seguintes tecnologias para frontend</p>
+<ul>
+    <!-- metemos o for no elemento que vai ser repetido -->
+    <!-- fazemos assim pq e um array de objetos linguagem é o nome do valor definido em  baixo -->
+    <!--mesma coisa para o id, definido em vaixo-->
+    <li v-for="lang in frontend" :key="lang.id">{{lang.linguagem}}</li>
 </ul>
 <div>
     <!-- equivalente ao onclick, quando clico no botao ele mostra o email ao chamar o metodo showEmail-->
@@ -40,7 +48,15 @@ export default {
             mostrar_email: false,
             email: 'mati@g.c',
             meuLink: 'https://google.com',
-            textoBotao:'Mostrar e-mail'
+            textoBotao:'Mostrar e-mail',
+            //aqui fazemos tudo como string
+            backend: ['Java', 'Python', 'SQL', 'JavaScript'],
+            //aqui fazemos como objetos em vez de string
+            frontend: [
+                {id: 1, linguagem: 'HTML'},
+                {id: 2, linguagem: 'CSS'},
+                {id: 3, linguagem: 'Vue'}
+            ]
         }
     },
     methods: {
