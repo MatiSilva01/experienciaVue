624a8f4a69f18b556cbe0a2cd5eb5ca47b9a25b9
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-26 12:08:55 +0000

    v-if v-else e v-show

--- aulas vue/2_projetovue/src/components/Info.vue
+++ aulas vue/2_projetovue/src/components/Info.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-    <p>Estou trabalhando no momento </p>
+    <!--um if, so mostra esse paragrafo e a variavel esta trabalhando for = true-->
+    <p v-if="esta_trabalhando">Estou trabalhando no momento </p>
+    <p v-else>Esta desempregado</p>
+        <!--o else tem de estar em baixo do if -->
 <p> Utilizo as seguintes tecnologias </p>
 <ul>
 <li>JavaScript</li>
@@ -8,11 +11,22 @@
 <li>Java</li>
 <li>SQL</li>
 </ul>
+<!--se true mostra email, so mostra se true, e mais simples-->
+<p v-show="mostrar_email">Mande uma mensagem para {{email}}</p>
+<!--o que esta la dentro das aspas é interpretado como javascript -->
+<p v-if="4 > 2">Teste</p>
 </div>
 
 </template>
 <script>
 export default {
-    name: 'InfoI'
+    name: 'InfoI',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'mati@g.c'
+        }
+    }
 }
 </script>
