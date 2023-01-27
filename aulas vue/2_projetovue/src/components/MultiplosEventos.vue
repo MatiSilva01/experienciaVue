98ed525c42edb2710ad3b09611948b2f6a05b95c
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-27 14:19:23 +0000

    chamar multiplos eventos/funcoes em apenas um click

---
+++ aulas vue/2_projetovue/src/components/MultiplosEventos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!--chama mais do que um metodo-->
+        <button @click="primeiro($event), segundo($event)">Ativar multiplos eventos</button>
+        <p> {{ multiplosEventos }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MultiplosEventos',
+    data(){
+        return {
+            multiplosEventos: ""
+        }
+    }, methods: {
+        primeiro(e){
+            e
+            this.multiplosEventos = "primeiro evento ativado"
+        }, 
+        segundo(e){
+            e
+            setTimeout(() => this.multiplosEventos="segundo evento ativado", 500)
+        }
+    }
+}
+</script>
+<style scoped>
+    div {
+        margin: 100px;
+    }
+</style>
--- aulas vue/2_projetovue/src/components/primeiroComponente.vue
+++ aulas vue/2_projetovue/src/components/primeiroComponente.vue
@@ -2,11 +2,16 @@
 <div>
     <h1>Ola Vue</h1>
     <p>O meu nome é {{ nome }} e sou {{ profissao }} </p>
+<MultiplosEventos />
 </div>
 </template>
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
 export default ({
     name: 'primeiroComponente',
+    components: {
+        MultiplosEventos
+    },
     data() {
          return {
             //o retorno dos dados e podem ser usados dentro do componente
