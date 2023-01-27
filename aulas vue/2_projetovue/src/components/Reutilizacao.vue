0feda8fb9a11549c3a9ef58690adca3b25caa16e
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-27 20:48:34 +0000

    reutilizacao de botao

---
+++ aulas vue/2_projetovue/src/components/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="ativar">Clique aqui</button>
+        <span v-show="ativou">Ativado</span>
+    </div>
+</template>
+<script>
+
+export default({
+    name: 'ReutilizacaoR',
+    data() {
+        return {
+            ativou: false
+        }
+    },
+    methods: {
+        ativar() {
+            //mete o contrario do q ta
+            this.ativou = !this.ativou
+        }
+    }
+})
+</script>
--- aulas vue/2_projetovue/src/components/primeiroComponente.vue
+++ aulas vue/2_projetovue/src/components/primeiroComponente.vue
@@ -3,14 +3,19 @@
     <h1>Ola Vue</h1>
     <p>O meu nome é {{ nome }} e sou {{ profissao }} </p>
 <MultiplosEventos />
+<ReutilizacaoR />
+<!-- se eu pusesse varios botoes ReutilizacaoR ele apenas punha ativado
+a esse botao! NAO A todos, mesmo q estejam em paginas diferentes-->
 </div>
 </template>
 <script>
 import MultiplosEventos from './MultiplosEventos.vue'
+import ReutilizacaoR from './Reutilizacao.vue';
 export default ({
     name: 'primeiroComponente',
     components: {
-        MultiplosEventos
+        MultiplosEventos,
+        ReutilizacaoR
     },
     data() {
          return {
