91835dba84fce5647e148cdc90407d8943b7eef4
Matilde Silva <matildeferreirasilva@gmail.com>
2023-01-30 21:47:39 +0000

    emit

--- aulas vue/2_projetovue/src/components/Picture.vue
+++ aulas vue/2_projetovue/src/components/Picture.vue
@@ -1,16 +1,29 @@
 <template>
 <!--v-bind é a mesma coisa que por so : -->
     <img :src="avatar" :alt="descricao">
+    <!--dentro das "" é o nome de um metodo-->
+    <MudarImagem @mudar-imagem="mudarImagemM"/>
 </template>
 <!--export default pq eu vou querer utilizar em outros lugares -->
 
 <script>
+import MudarImagem from './MudarImagem.vue'
 export default {
     name: 'PictureI',
+    components: {
+        MudarImagem
+    },
     data() {
         return {
             avatar: "/img/IMG_8898.JPG",
             decricao: "Maatilde"
+        }
+    },
+    methods: {
+        mudarImagemM(){
+            console.log('oioi')
+            this.avatar = "/img/User_icon_2.svg.png"
+
         }
     }
 
