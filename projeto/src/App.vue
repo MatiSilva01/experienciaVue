98ba5e8af11cfcc68e2e09b5fa8115882c762b6d
Matilde Silva <matildeferreirasilva@gmail.com>
2023-02-01 00:55:02 +0000

    routes

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,30 +1,27 @@
+<!--inicializacao do aplicativo, presente em todas as views
+entao meto aqui os elementos q se repetem-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <!--significa que a app vai utilizar o navbar-->
+  <Navbar />
+  <Footer />
+  
+  <!--vai buscar a view especifica-->
   <router-view/>
 </template>
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+export default {
+  //os componentes que a app vai ter
+  components: {
+    Navbar,
+    Footer
+  }
+  
+}
+</script>
+
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ projeto/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <!--logotipo e um elemento global entao vai ser mandado
+            atraves do app-->
+            <img src="" id="logo">
+        </router-link>
+        <!-- to para onde ele redireciona-->
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+<script>
+
+export default {
+    name: "Navbar"
+
+}
+
+</script>
+<style scoped>
+
+</style>
