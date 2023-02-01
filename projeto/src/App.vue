81022d3249a6e2282a9e2ba26dd51aab5ae66c9b
Matilde Silva <matildeferreirasilva@gmail.com>
2023-02-01 11:04:50 +0000

    footer e nav

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -2,11 +2,11 @@
 entao meto aqui os elementos q se repetem-->
 <template>
   <!--significa que a app vai utilizar o navbar-->
-  <Navbar />
-  <Footer />
-  
+  <!-- na pagina de navbar nos dizemos q ela recebe o logo e o alt entao mandamos aqui-->
+  <Navbar :logo="logo_src" :alt="app_name" />
   <!--vai buscar a view especifica-->
   <router-view/>
+  <Footer />
 </template>
 <script>
 import Navbar from './components/Navbar.vue'
@@ -16,6 +16,14 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data(){
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make your burguer"
+
+    }
+
   }
   
 }
@@ -23,5 +31,23 @@ export default {
 
 
 <style>
+/* para todos os q nao scoped */
+*{
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+/*para classes*/
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 
 </style>
--- projeto/src/components/Navbar.vue
+++ projeto/src/components/Navbar.vue
@@ -1,23 +1,52 @@
 <template>
-    <nav>
-        <router-link to="/">
+    <div id="nav">
+        <router-link to="/" id="logo_url">
             <!--logotipo e um elemento global entao vai ser mandado
             atraves do app-->
-            <img src="" id="logo">
+            <!-- os: para ser dinamico e ir ao app buscar o logo e o alt-->
+            <img :src="logo" :alt="alt" id="logo">
         </router-link>
         <!-- to para onde ele redireciona-->
         <router-link to="/">Home</router-link> |
         <router-link to="/pedidos">Pedidos</router-link>
-    </nav>
+    </div>
 </template>
 <script>
 
 export default {
-    name: "Navbar"
+    name: "Navbar",
+    props: ["logo", "alt"]
 
 }
 
 </script>
 <style scoped>
+#nav{
+    background-color: #222;
+    border-bottom: 4px;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+#nav #logo_url{
+    margin:auto;
+    margin-left: 0;
+}
 
+#logo {
+    width: 40px;
+    height: 40px;
+}
+
+#nav a{
+    color: #fcba03;
+    text-decoration: none;
+    margin: 12px;
+    transition: 0.5s;
+}
+
+#nav a:hover {
+    color: #FFF;
+}
 </style>
